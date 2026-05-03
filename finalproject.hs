1f8e798a614399e3df30b9a749af33f716632602
lbrookskanost <142836967+lbrookskanost@users.noreply.github.com>
2026-05-03 15:01:27 -0500

    first round of cleaning up everything; renamed lang

--- finalproject.hs
+++ finalproject.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE GADTs,FlexibleContexts #-}
 {-# OPTIONS_GHC -Wno-missing-methods #-}
-{-
-boilerplate necessities
+
+import Control.Monad
+
+--boilerplate necessities
 T ::= int 
     | bool 
     | id 
@@ -12,151 +14,215 @@ T ::= int
     | T ^ T 
     | between T T T 
     | lambda (id:TY) in T 
-    | T T  --pairs
-    | bind id T T 
+    | T T  
     | if T then T else T 
     | T && T 
     | T || T 
     | T <= T 
     | isZero T 
-    | Fix T --?
+    | Fix T 
 
 TY ::= Num 
      | Boolean 
      | TY -> TY
--}
-
--- Abstract Syntax Definitions; ripped from p4 and modified --
 
 --Internal (may need to move some of these around) --
-data KULang where
-    Num :: Int -> KULang  
-    Plus :: KULang -> KULang -> KULang 
-    Minus :: KULang -> KULang -> KULang
-    Mult :: KULang -> KULang -> KULang 
-    Div :: KULang -> KULang -> KULang  
-    Exp :: KULang -> KULang -> KULang
-    Between :: KULang -> KULang -> KULang -> KULang 
-    Lambda :: String -> KULang -> KULang 
-    If :: KULang -> KULang -> KULang -> KULang
-    And :: KULang -> KULang -> KULang
-    Or :: KULang -> KULang -> KULang
-    Leq :: KULang -> KULang -> KULang
-    IsZero :: KULang -> KULang 
-    Id :: String -> KULang
-    App :: KULang -> KULang -> KULang
+data Cb where
+    Num :: Int -> Cb  
+    Bool :: Boolean -> Cb
+    Plus :: Cb -> Cb -> Cb 
+    Minus :: Cb -> Cb -> Cb
+    Mult :: Cb -> Cb -> Cb 
+    Div :: Cb -> Cb -> Cb  
+    Exp :: Cb -> Cb -> Cb
+    Between :: Cb -> Cb -> Cb -> Cb 
+    Lambda :: String -> Cb -> Cb 
+    App :: Cb -> Cb -> Cb
+    Id :: String -> Cb
+    If :: Cb -> Cb -> Cb -> Cb
+    And :: Cb -> Cb -> Cb
+    Or :: Cb -> Cb -> Cb
+    Leq :: Cb -> Cb -> Cb
+    IsZero :: Cb -> Cb 
+    Fix :: Cb -> Cb
     deriving (Show,Eq)
 
 --Data Types--
-data KULangVal where
-    NumV :: Int -> KULangVal
-    BoolV :: Bool -> KULangVal
-    ClosureV :: String -> KULang -> EnvVal -> KULangVal
+data CbVal where
+    NumV :: Int -> CbVal
+    BoolV :: Bool -> CbVal
+    ClosureV :: String -> Cb -> EnvVal -> CbVal
     deriving (Show,Eq)
 
 --External--
-data KULangExt where
-    NumX :: Int -> KULangExt
-    PlusX :: KULangExt -> KULangExt -> KULangExt
-    MinusX :: KULangExt -> KULangExt -> KULangExt
-    MultX :: KULangExt -> KULangExt -> KULangExt
-    DivX :: KULangExt -> KULangExt -> KULangExt
-    ExpX :: KULangExt -> KULangExt -> KULangExt
-    If0X :: KULangExt -> KULangExt -> KULangExt -> KULangExt
-    LambdaX :: String -> KULangExt -> KULangExt
-    AppX :: KULangExt -> KULangExt -> KULangExt 
-    BindX :: String -> KULangExt -> KULangExt -> KULangExt
-    IdX :: String -> KULangExt
+data CbExt where
+    NumX :: Int -> CbExt
+    PlusX :: CbExt -> CbExt -> CbExt
+    MinusX :: CbExt -> CbExt -> CbExt
+    MultX :: CbExt -> CbExt -> CbExt
+    DivX :: CbExt -> CbExt -> CbExt
+    ExpX :: CbExt -> CbExt -> CbExt
+    If0X :: CbExt -> CbExt -> CbExt -> CbExt
+    LambdaX :: String -> CbExt -> CbExt
+    AppX :: CbExt -> CbExt -> CbExt 
+    BindX :: String -> CbExt -> CbExt -> CbExt
+    IdX :: String -> CbExt
     deriving (Show,Eq)
 
 -- Environment Definitions
-type Env = [(String,KULang)]
-type EnvVal = [(String,KULangVal)]
+type EnvVal = [(String, CbVal)]
+type Cont = [(String, CbTy)]
 
--- Reader Definition
+-- Reader & Helper Methods (boilerplate from p5)
 data Reader e a = Reader (e -> Maybe a)
-
--- Monad Definition
-instance Monad (Reader e) where
-    g >>= f = Reader $ \e -> 
-        case runR g e of
-            Nothing -> Nothing
-            Just x -> runR (f x) e
-
- -- Applicative Definition
-instance Applicative (Reader e) where
-  pure x = Reader $ \e -> Just x
-  (Reader f) <*> (Reader g) = Reader $ \e -> case f e of
-    Nothing -> Nothing
-    Just fx -> case g e of
-      Nothing -> Nothing
-      Just gx -> Just (fx gx)
-
--- Functor Definition
-instance Functor (Reader e) where
- fmap f (Reader g) = Reader $ \e -> case g e of
-  Nothing -> Nothing
-  Just x -> Just (f x)
-
--- Fail Definition
-instance MonadFail (Reader e) where
-  fail _ = Reader $ \_ -> Nothing
-
--- Helper Methods
-runR :: Reader e a -> e -> Maybe a
-runR (Reader f) e = f e 
 
 ask :: Reader a a 
 ask = Reader $ \e -> Just e
 
-local :: (e->t) -> Reader t a -> Reader e a
-local f r = Reader $ \e -> (runR r (f e))
+runR :: Reader e a -> e -> Maybe a
+runR (Reader f) e = f e 
+
+local :: (e -> t) -> Reader t a -> Reader e a
+local f r = Reader $ \e -> runR r (f e)
 
 useClosure :: String -> KULangVal -> EnvVal -> EnvVal -> EnvVal
-useClosure i v e _ = (i,v):e 
+useClosure i v e _ = (i,v):e
+
+instance Monad (Reader e) where
+    g >>= f = Reader $ \e -> 
+        case runR g e of
+      Nothing -> Nothing
+      Just v  -> runR (f v) e
+
+instance Functor (Reader e) where
+    fmap f (Reader g) = Reader $ \e ->
+        case g e of
+        Nothing -> Nothing
+        Just v  -> Just (f v)
+
+instance Applicative (Reader e) where
+    pure x = Reader $ \e -> Just x
+    (Reader f) <*> (Reader g) = Reader $ \e ->
+        case f e of
+      Nothing -> Nothing
+      Just h  ->
+        case g e of
+          Nothing -> Nothing
+          Just x  -> Just (h x)
+
+instance MonadFail (Reader e) where
+  fail _ = Reader $ \_ -> Nothing
 
 --more boilerplate to edit; make sure these are actually CORRECT--
-elabTerm :: KULangExt -> KULang 
-elabTerm (NumX x) = (Num x)
-elabTerm (PlusX l r) = (Plus (elabTerm l) (elabTerm r))
-elabTerm (MinusX l r) = (Minus (elabTerm l) (elabTerm r))
-elabTerm (MultX l r) = (Mult (elabTerm l) (elabTerm r))
-elabTerm (DivX l r) = (Div (elabTerm l) (elabTerm r))
-elabTerm (ExpX b e) = (Exp (elabTerm b) (elabTerm e))
-elabTerm (If0X c t e) = (If0 (elabTerm c) (elabTerm t) (elabTerm e))
-elabTerm (LambdaX i b) = (Lambda i (elabTerm b))
-elabTerm (AppX f a) = (App (elabTerm f) (elabTerm a)) 
-elabTerm (BindX i v b) = App (Lambda i (elabTerm b))(elabTerm v) 
-elabTerm (IdX id) = (Id id)
-
-evalReader :: KULang -> Reader EnvVal KULangVal
---this is static still
---none require environments
-evalReader (Num x) = return (NumV x)
-evalReader (Plus l r) = do {(NumV l') <- (evalReader l);
-  (NumV r') <- (evalReader  r);
-  return (NumV (l' + r'))}  
-evalReader (Minus l r) = do {(NumV l') <- (evalReader l);
-  (NumV r') <- (evalReader  r);
-  return (NumV (l' - r'))}
-evalReader (Mult l r) = do {(NumV l') <- (evalReader  l);
-  (NumV r') <- (evalReader  r);
-  return (NumV (l' * r'))} 
-evalReader (Div l r) = do {(NumV l') <- (evalReader l); 
-  (NumV r') <- (evalReader  r);
-  if r' == 0 then fail "div by 0" else return (NumV (l' `div` r'))}  
-evalReader (Exp b e) = do {(NumV b') <- (evalReader  b);
-  (NumV e') <- (evalReader  e);
-  if e < 0 then fail "negative exponent" else return (NumV (b'^e')}
-evalReader (If0 c t e) = do {(NumV c') <- (evalReader c);
-  if c' == 0 then (evalReader  t) else (evalReader  e)}
+typeof :: Cb -> Reader Cont CbTy
+typeof (Num x) = return TNum
+typeof (Boolean x) = return TBool
+typeof (Id id) = do { cont <- ask;
+  case lookup id cont of
+    Just t -> return t
+    Nothing -> fail "unbound id"}
+typeof (Plus l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TNum && r'==TNum) then return TNum else fail "type fail in plus")}  
+typeof (Minus l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TNum && r'==TNum) then return TNum else fail "type fail in minus")}  
+typeof (Mult l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TNum) && (r'==TNum) then return TNum else fail "type fail in mult")  } 
+typeof (Div l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TNum && r'==TNum) then return TNum else fail "type fail in div")  } 
+typeof (Exp l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TNum && r'==TNum) then return TNum else fail "type fail in exp")  }  
+typeof (Between l m r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  m' <- (typeof m);
+  (if (l'==TNum && r'==TNum && m'==TNum) then return TNum else fail "type fail in between")  }  
+typeof (Lambda x t b) = do {
+  b' <- local((x,t):) (typeof b);
+  return (t :->: b')}  
+typeof (App f a) = do {
+  (fD :->: fR) <- typeof f;
+  a' <- typeof a;
+  if fD == a' then return fR else fail "type fail in app"} 
+typeof (If c t e) = do {c' <- (typeof c);
+  t' <- (typeof t);
+  e' <- (typeof e);
+  (if (c' == TBool && (t' == e')) then return t' else fail "type fail in if") }   
+typeof (And l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TBool && r'==TBool) then return TBool else fail "type fail in and")  }  
+typeof (Or l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  (if (l'==TBool && r'==TBool) then return TBool else fail "type fail in or")  }
+typeof (IsZero x) = do {x' <- (typeof x);
+  (if (x' == TNum) then return TBool else fail "type fail in iszero")  }
+typeof (Leq l r) = do {l' <- (typeof l);
+  r' <- (typeof r);
+  if (r' == TNum && l' == TNum) then return TBool else fail "type fail in leq"}
+typeof (Fix f) = do {(d :->: r) <- (typeof f);
+  return r}
   
-evalReader (Id id) = do { env <- ask;
+elab :: CbExt -> Cb 
+elab (NumX x) = (Num x)
+elab (PlusX l r) = (Plus (elab l) (elab r))
+elab (MinusX l r) = (Minus (elab l) (elab r))
+elab (MultX l r) = (Mult (elab l) (elab r))
+elab (DivX l r) = (Div (elab l) (elab r))
+elab (ExpX b e) = (Exp (elab b) (elab e))
+elab (If0X c t e) = (If0 (elab c) (elab t) (elab e))
+elab (LambdaX i b) = (Lambda i (elab b))
+elab (AppX f a) = (App (elab f) (elab a)) 
+elab (BindX i v b) = App (Lambda i (elab b))(elab v) 
+elab (IdX id) = (Id id)
+
+eval :: Cb -> Reader EnvVal CbVal
+eval (Num x) = return (NumV x)
+eval (Boolean x) = return (BooleanV x)
+eval (Id id) = do { env <- ask;
   case (lookup id env) of
     Just x -> return x
     Nothing -> fail "unbound id"}
-evalReader (Lambda i b) = do { env <- ask;
-return (ClosureV i b env)}
-evalReader (App f a) = do {(ClosureV i b e) <- (evalReader f);
-  a' <- (evalReader a);
-  local (useClosure i a' e)(evalReader b) } 
+eval (Plus l r) = do {(NumV l') <- (eval l);
+  (NumV r') <- (eval  r);
+  return (NumV (l' + r'))}  
+eval (Minus l r) = do {(NumV l') <- (eval l);
+  (NumV r') <- (eval  r);
+  return (NumV (l' - r'))}
+eval (Mult l r) = do {(NumV l') <- (eval  l);
+  (NumV r') <- (eval  r);
+  return (NumV (l' * r'))} 
+eval (Div l r) = do {(NumV l') <- (eval l); 
+  (NumV r') <- (eval  r);
+  if r' == 0 then fail "div by 0" else return (NumV (l' `div` r'))}  
+eval (Exp b e) = do {(NumV b') <- (eval  b);
+  (NumV e') <- (eval  e);
+  if e' < 0 then fail "negative exponent" else return (NumV (b' ^ e'))}
+eval (Between l m r) = do { (NumV l') <- (eval l);
+  (NumV m') <- (eval m);
+  (NumV r') <- (eval r);
+  return (BooleanV ((m' > l') && (m' < r')))}
+eval (Lambda i t b) = do { env <- ask;
+  return (ClosureV i t b env)}
+eval (App f a) = do {(ClosureV i t b e) <- (eval f);
+  a' <- (eval a);
+  local (useClosure i a' e)(eval b) } 
+eval (If c t e) = do {(BooleanV c') <- (eval c);
+  if c' then (eval  t) else (eval  e)}
+eval (And l r) = do { (BooleanV l') <- (eval l);
+  (BooleanV r') <- (eval r);
+  return (BooleanV (l' && r'))}
+eval (Or l r) = do { (BooleanV l') <- (eval l);
+  (BooleanV r') <- (eval r);
+  return (BooleanV (l' || r'))} --is this how or works in haskell? no idea
+eval (IsZero x) = do { (NumV x') <- (eval x);
+  return (BooleanV (x' == 0))}
+eval (Leq l r) = do {(NumV l') <- (eval l);
+  (NumV r') <- (eval r);
+  return (BooleanV (l' <= r'))}
+eval (Fix f) = do 
+  (ClosureV i t b e) <- (eval f)
+  let fixVal = ClosureV i t b ((i, fixVal):e)
+  local (const ((i, fixVal):e))(eval b)
